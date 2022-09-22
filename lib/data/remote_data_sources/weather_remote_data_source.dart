368f83a93e8739925bcf0e0750bbed12fa936083
Maciej Nosek <macnosek@gmail.com>
2022-09-22 18:50:13 +0200

    Data Source impl

---
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+class WeatherRemoteDataSource {
+  Future<Map<String, dynamic>?> getWeatherData({
+    required String city,
+  }) async {
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=0777b23407424fe7820115536221204&q=$city&aqi=no');
+
+    return response.data;
+  }
+}
